3b9592e52f8a63b0b90d05b7897f09b084094b73
KYHSGeekCode <jourhyang123@gmail.com>
2018-10-22 00:48:52 +0900

    Changed to bluelog

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# LogCalls
-Loggs method calls of apps using xposed framework
+# LogBluetooth
+Loggs bluetooth API calls of apps using xposed framework. Change the first empty string to your target package name. (The equals("")at the first of HandleLoadPackage())
--- app/src/main/java/com/kyunggi/logcalls/Main.java
+++ app/src/main/java/com/kyunggi/logcalls/Main.java
@@ -14,135 +14,200 @@ import android.app.*;
 
 public class Main implements IXposedHookLoadPackage
 {
-	private String TAG="LogCall";
+	private String TAG="LogBluetooth";
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
 	{
-		if (!lpparam.packageName.equals("com.android.bluetooth") && !lpparam.packageName.equals("javax.obex")/*&&!lpparam.packageName.equals("org.codeaurora.bluetooth")*/)
+		if (!lpparam.packageName.equals("") && !lpparam.packageName.equals("javax.obex"))
 		{
 			//Log.v(TAG, "Not: " + lpparam.packageName);
 			return;
 		}
-	
+
 		Log.i(TAG, "Yes " + lpparam.packageName);
-		TAG+=lpparam.packageName;
+		TAG += lpparam.packageName;
 		//Modified https://d3adend.org/blog/?p=589
-		ApplicationInfo applicationInfo =lpparam.appInfo;//AndroidAppHelper.currentApplicationInfo();
-		if(applicationInfo==null)
+	/*	ApplicationInfo applicationInfo =lpparam.appInfo;//AndroidAppHelper.currentApplicationInfo();
+		if (applicationInfo == null)
 		{
-			Log.e(TAG,"AppInfo null!!");
+			Log.e(TAG, "AppInfo null!!");
 			return;
 		}
-		if (applicationInfo.processName.equals("com.android.bluetooth"))
-		{		
-			Set<String> classes = new HashSet<>();
-			DexFile dex;
-			try
-			{
-				dex = new DexFile(applicationInfo.sourceDir);
-				Enumeration entries = dex.entries();
-				while (entries.hasMoreElements())
-				{
-					String entry = (String) entries.nextElement();
-					classes.add(entry);
-				}
-				dex.close();
-			} 
-			catch (IOException e)
-			{
-				Log.e(TAG, e.toString());
-			}
+		Log.i(TAG, "succ 35");
+		/*
+		 //if (applicationInfo.processName.equals("com.android.bluetooth"))
+		 {		
+		 Set<String> classes = new HashSet<>();
+		 DexFile dex;
+		 try
+		 {
+		 dex = new DexFile(applicationInfo.sourceDir);
+		 Enumeration entries = dex.entries();
+		 while (entries.hasMoreElements())
+		 {
+		 String entry = (String) entries.nextElement();
+		 classes.add(entry);
+		 }
+		 dex.close();
+		 } 
+		 catch (IOException e)
+		 {
+		 Log.e(TAG, e.toString());
+		 }
 
-			for (String className : classes)
+		 for (String className : classes)
+		 {
+		 boolean obex=false;
+		 if (className.startsWith("android.bluetooth") || (obex = className.startsWith("javax.obex"))/*||className.startsWith("org.codeaurora.bluetooth"))
+		 {
+		 try
+		 {
+		 final Class clazz = lpparam.classLoader.loadClass(className);
+		 for (final Method method : clazz.getDeclaredMethods())
+		 {
+		 */
+		
+		/*if (Modifier.isAbstract(method.getModifiers()))
+		{
+			continue;  //Avoid hooking abstract methods
+		}
+		if (obex)
+		{
+			if (!Modifier.isPublic(method.getModifiers()))
 			{
-				boolean obex=false;
-				if (className.startsWith("com.android.bluetooth") || (obex = className.startsWith("javax.obex"))||className.startsWith("org.codeaurora.bluetooth"))
+				continue;
+			}			
+		}
+		*/
+		ClassLoader sysClsLoader=lpparam.classLoader.getSystemClassLoader();
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothOutputStream",sysClsLoader,"close", new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
 				{
-					try
+					Log.i(TAG,"BTOutputStream.Close() called");
+				}
+		});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothOutputStream",sysClsLoader,"write", int.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BTOutputStream.write("+param.args[0]+")");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothOutputStream",sysClsLoader,"write", byte[].class, int.class, int.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BTOutputStream.write("+Arrays.toString((byte[])param.args[0])+","+param.args[1]+","+param.args[2]+")");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothOutputStream",sysClsLoader,"flush", new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BTOutputStream.flush() called");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothInputStream",sysClsLoader,"available", new XC_MethodHook(){
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Object o=param.getResult();
+					if(o!=null)
 					{
-						final Class clazz = lpparam.classLoader.loadClass(className);
-						for (final Method method : clazz.getDeclaredMethods())
-						{
-							if(Modifier.isAbstract( method.getModifiers()))
-							{
-								continue;  //Avoid hooking abstract methods
-							}
-							if (obex)
-							{
-								if (!Modifier.isPublic(method.getModifiers()))
-								{
-									continue;
-								}			
-							}
-							XposedBridge.hookMethod(method, new XC_MethodHook() {
-									final String methodNam=method.getName();
-									final String classNam=clazz.getName();
-									final StringBuilder sb=new StringBuilder("[");
-									final String logstr=classNam + "." + methodNam;
-									final boolean isVoid=method.getReturnType().equals(Void.TYPE);
-									final String proto=method.getReturnType().toString() + " " + logstr;
-									final String voidRet=proto + " returned ";
-									final String nullRet=voidRet + "null";
-									@Override
-									protected void beforeHookedMethod(MethodHookParam param) throws Throwable
-									{
-										//Method method=(Method)param.args[0];
-										sb.setLength(0);
-										sb.append(logstr);
-										//Log.v(TAG,logstr);
-										if (param.args != null)
-										{
-											for (Object o:param.args)
-											{
-												String typnam="";
-												String value="null";
-												if (o != null)
-												{
-													typnam = o.getClass().getName();
-													value = o.toString();
-												}
-												sb.append(typnam).append(" ").append(value).append(", ");
-											}
-											
-										}
-										sb.append("]");
-										Log.v(TAG, sb.toString());
-									}
-									@Override
-									protected void afterHookedMethod(MethodHookParam param) throws Throwable
-									{
-										Object o=null;
-										try
-										{
-											o=param.getResultOrThrowable();
-										}
-										catch (Throwable e)
-										{
-											Log.v(TAG,logstr,e);
-											param.setResult(e);
-											return;
-										}
-										if (isVoid)
-										{
-											Log.v(TAG, voidRet);
-											return;
-										}
-										if (o == null)
-										{
-											Log.v(TAG, nullRet);
-											return;
-										}
-										Log.v(TAG, voidRet + o.toString());
-									}
-								});
-						}
+						Log.i(TAG,"BTInputStream.available()="+(int)o);
 					}
-					catch (ClassNotFoundException e)
-					{
-						Log.wtf(TAG, e.toString());
+					else{
+						Log.i(TAG,"BTInputStream.available() may have thrown an exception");
 					}
 				}
-			}
-		}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothInputStream",sysClsLoader,"close", new XC_MethodHook(){
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BTInputStream.close()");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothInputStream",sysClsLoader,"read", new XC_MethodHook(){
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Object o=param.getResult();
+					if(o!=null)
+						Log.i(TAG,"BTInputStream.read() ret:"+(int)o);
+					else
+						Log.i(TAG,"BTInputStream.read() may have thrown an exception");
+					
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothInputStream",sysClsLoader,"read",byte[].class, int.class, int.class, new XC_MethodHook(){
+				@Override
+				protected void afterHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Object o=param.getResult();
+					if(o!=null){
+						Log.i(TAG,"BTInputStream.read(buf,"+param.args[1]+","+param.args[2]+") ret:"+(int)o);
+						if(param.args[0]!=null)
+							Log.i(TAG,"buf="+Arrays.toString((byte[])param.args[0]));
+						else
+							Log.i(TAG,"buf is null;;;;;");
+					}
+					else
+						Log.i(TAG,"BTInputStream.read(buf,"+param.args[1]+","+param.args[2]+") may have thrown an exception");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothDevice",sysClsLoader,"createInsecureRfcommSocket",int.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BluetoothDeice.createInsecureRfcommSocket("+param.args[0]+")called");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothDevice",sysClsLoader,"createInsecureRfcommSocketToServiceRecord",UUID.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BluetoothDeice.createInsecureRfcommSocketToServiceRecord(UUID="+((UUID)param.args[0]).toString()+")called");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothDevice",sysClsLoader,"createRfcommSocketToServiceRecord",UUID.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BluetoothDeice.createRfcommSocketToServiceRecord(UUID="+((UUID)param.args[0]).toString()+")called");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothDevice",sysClsLoader,"createInsecureL2capSocket",int.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BluetoothDeice.createInsecureL2capSocket(channel="+((int)param.args[0])+")called");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothDevice",sysClsLoader,"createL2capSocket",int.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BluetoothDeice.createL2capSocket(channel="+((int)param.args[0])+")called");
+				}
+			});
+		XposedHelpers.findAndHookMethod("android.bluetooth.BluetoothDevice",sysClsLoader,"createRfcommSocket",int.class, new XC_MethodHook(){
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable
+				{
+					Log.i(TAG,"BluetoothDeice.createRfcommSocket(channel="+((int)param.args[0])+")called");
+				}
+			});
+		
+		/*}
+		 }
+		 catch (ClassNotFoundException e)
+		 {
+		 Log.wtf(TAG, e.toString());
+		 }
+		 }
+		 }
+		 }*/
 		//	ClassLoader rootcl=lpparam.classLoader.getSystemClassLoader();
 		//findAndHookMethod("de.robv.android.xposed.XposedBridge", rootcl, "handleHookedMethod", Member.class, int.class, Object.class, Object.class, Object[].class, );
 	}
